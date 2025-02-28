8985ad4a90aea593632928f5b013ee73e2353b00
maxruther <34466604+maxruther@users.noreply.github.com>
2025-02-27 19:49:25 -0600

    Updated scrapers to collect two months
    Previously, it only scraped the current month. Now it scrapes the next one, too.

--- its_showtimes/scrapers/musicbox_scrape.py
+++ its_showtimes/scrapers/musicbox_scrape.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from bs4 import BeautifulSoup
 
@@ -26,23 +28,23 @@ def musicbox_scrape(driver: webdriver.Chrome,
     # Navigate the driver to the Music Box calendar page.
     mb_calendar_link = 'https://musicboxtheatre.com/films-and-events'
     driver.get(mb_calendar_link)
-    calendar_elem_xpath = '/html/body/div[1]/main/div/div[2]/div/div/div/div[2]'
-    calendar_text = driver.find_element(By.XPATH, calendar_elem_xpath).get_attribute('innerHTML')
+
+    # Check for second page of calendar, if it exists.
+    next_month_button_xpath = '/html/body/div[1]/main/div/div[2]/div/div/div/div[1]/div[2]/a[2]'
+    next_month_button_element = WebDriverWait(driver, 3).until(
+        EC.element_to_be_clickable((By.XPATH, next_month_button_xpath))
+    )
+
+    calendar_page_links = [mb_calendar_link]
+
+    if next_month_button_element:
+        next_month_cal_link = next_month_button_element.get_attribute('href')
+        calendar_page_links.append(next_month_cal_link)
+
 
     # # For testing purposes, load old calendar text from a pickle file.
     # with open('data/showtimes/test_mb_calendar_text.pkl', 'rb') as file:
     #     calendar_text = pickle.load(file)
-
-
-    # Time the imminent scraping by first
-    # noting its start (for the dev's reference.)
-    scrape_start = time.time()
-
-    soup = BeautifulSoup(calendar_text, 'html.parser')
-
-    # calendar_days = soup.select('div.Item')
-    # calendar_days = soup.select('td.InsideDate', limit=1)
-    calendar_days = soup.select('div.calendar-cell')
 
     films_showtimes = {}
     film_details = {}
@@ -52,80 +54,99 @@ def musicbox_scrape(driver: webdriver.Chrome,
         # 'Strange and Found',
         ]
 
-    for day in calendar_days:
-    # for day in calendar_days[27:32]:
-        # print(day.get('class'))
-        if 'calendar-head' in day.get('class'):
-            # print('EMPTY CALENDAR CELL - HEADER')
-            pass
-        elif 'empty' in day.get('class'):
-            # print('EMPTY CALENDAR CELL - PAST DATE')
-            pass
-        else:
-            calendar_date_elem = day.select_one('div.calendar-date')
-            date_text_all = calendar_date_elem.text.strip().split(', ')
-            date_str = ' '.join(date_text_all[-2:])
 
-            day_datetime = datetime.strptime(date_str, '%b %d %Y')
-            # print(f'\n\n{day_datetime}\n')
-            # print(f'\n\n{date_str}\n')
+    # Time the imminent scraping by first
+    # noting its start (for the dev's reference.)
+    scrape_start = time.time()
+
+    # Iterate through each calendar page, scraping the showtimes and film details.
+    for cal_link in calendar_page_links:
+        driver.get(cal_link)
+
+        calendar_elem_xpath = '/html/body/div[1]/main/div/div[2]/div/div/div/div[2]'
+        calendar_text = driver.find_element(By.XPATH, calendar_elem_xpath).get_attribute('innerHTML')
+        soup = BeautifulSoup(calendar_text, 'html.parser')
+
+        # calendar_days = soup.select('div.Item')
+        # calendar_days = soup.select('td.InsideDate', limit=1)
+        calendar_days = soup.select('div.calendar-cell')
 
         
-        shows = day.select('div.programming-content')
-        for show in shows:
 
-            show_title = show.select_one('a').text.strip()
+        for day in calendar_days:
+        # for day in calendar_days[27:32]:
+            # print(day.get('class'))
+            if 'calendar-head' in day.get('class'):
+                # print('EMPTY CALENDAR CELL - HEADER')
+                pass
+            elif 'empty' in day.get('class'):
+                # print('EMPTY CALENDAR CELL - PAST DATE')
+                pass
+            else:
+                calendar_date_elem = day.select_one('div.calendar-date')
+                date_text_all = calendar_date_elem.text.strip().split(', ')
+                date_str = ' '.join(date_text_all[-2:])
 
-            # Remove parenthesized release year from the show/film title string, if present.
-            pattern = r'(.*)\ \((\d{4})\)$'
-            if re.search(pattern, show_title):
-                show_title = re.search(pattern, show_title).groups()[0]
+                day_datetime = datetime.strptime(date_str, '%b %d %Y')
+                # print(f'\n\n{day_datetime}\n')
+                # print(f'\n\n{date_str}\n')
 
-            show_tags = show.select_one('div.tags').text.strip()
-            if show_tags:
-                """
-                # Series Handling
-                if show_tags == 'Film Series':
-                    series_relative_link = show.select_one('a').get('href')
-                    print(series_relative_link)
-                    series_link = 'https://musicboxtheatre.com' + series_relative_link
-                    driver.get(series_link)
-                """
+            
+            shows = day.select('div.programming-content')
+            for show in shows:
 
-            showtimes_elem = show.select_one('div.programming-showtimes')
+                show_title = show.select_one('a').text.strip()
 
-            if showtimes_elem:
+                # Remove parenthesized release year from the show/film title string, if present.
+                pattern = r'(.*)\ \((\d{4})\)$'
+                if re.search(pattern, show_title):
+                    show_title = re.search(pattern, show_title).groups()[0]
 
-                if show_title not in film_details:
-                    if show_title not in mb_series_list:
-                        show_rel_link = show.select_one('a').get('href')
-                        show_link = 'https://musicboxtheatre.com' + show_rel_link
-                        # print(show_link)
-                        driver.get(show_link)
-                        main_section_xpath = '/html/body/div[1]/main'
-                        main_section_text = driver.find_element(By.XPATH, value=main_section_xpath).get_attribute('innerHTML')
-                        main_section_soup = BeautifulSoup(main_section_text, 'html.parser')
+                show_tags = show.select_one('div.tags').text.strip()
+                if show_tags:
+                    """
+                    # Series Handling
+                    if show_tags == 'Film Series':
+                        series_relative_link = show.select_one('a').get('href')
+                        print(series_relative_link)
+                        series_link = 'https://musicboxtheatre.com' + series_relative_link
+                        driver.get(series_link)
+                    """
 
-                        tech_summary_elem = main_section_soup.select_one('p.tech-summary')
-                        tech_summ_deets = tech_summary_elem.select('span')
-                        tech_deet_list = [deet.text.strip() for deet in tech_summ_deets]
-                        film_details[show_title] = tech_summary_list_to_dict(tech_deet_list)           
+                showtimes_elem = show.select_one('div.programming-showtimes')
 
-                showtimes = showtimes_elem.select('a.use-ajax')
-                # print(f'Showtimes:')
-                for showtime in showtimes:
-                    showtime_str = showtime.text.replace('"', '')
+                if showtimes_elem:
 
-                    date_and_showtime = date_str + ' ' + showtime_str
-                    showtime_datetime = datetime.strptime(date_and_showtime, 
-                                                        '%b %d %Y %I:%M%p')
-                    # print(showtime_datetime)
+                    if show_title not in film_details:
+                        if show_title not in mb_series_list:
+                            show_rel_link = show.select_one('a').get('href')
+                            show_link = 'https://musicboxtheatre.com' + show_rel_link
+                            # print(show_link)
+                            driver.get(show_link)
+                            main_section_xpath = '/html/body/div[1]/main'
+                            main_section_text = driver.find_element(By.XPATH, value=main_section_xpath).get_attribute('innerHTML')
+                            main_section_soup = BeautifulSoup(main_section_text, 'html.parser')
 
-                    if show_title not in films_showtimes:
-                        films_showtimes[show_title] = [showtime_datetime]
-                    else:
-                        if showtime_datetime not in films_showtimes[show_title]:
-                            films_showtimes[show_title].append(showtime_datetime)
+                            tech_summary_elem = main_section_soup.select_one('p.tech-summary')
+                            tech_summ_deets = tech_summary_elem.select('span')
+                            tech_deet_list = [deet.text.strip() for deet in tech_summ_deets]
+                            film_details[show_title] = tech_summary_list_to_dict(tech_deet_list)           
+
+                    showtimes = showtimes_elem.select('a.use-ajax')
+                    # print(f'Showtimes:')
+                    for showtime in showtimes:
+                        showtime_str = showtime.text.replace('"', '')
+
+                        date_and_showtime = date_str + ' ' + showtime_str
+                        showtime_datetime = datetime.strptime(date_and_showtime, 
+                                                            '%b %d %Y %I:%M%p')
+                        # print(showtime_datetime)
+
+                        if show_title not in films_showtimes:
+                            films_showtimes[show_title] = [showtime_datetime]
+                        else:
+                            if showtime_datetime not in films_showtimes[show_title]:
+                                films_showtimes[show_title].append(showtime_datetime)
 
     # # Printing the various outputs of the scrape.
     # for pair in film_details.items():
@@ -173,4 +194,4 @@ if __name__ == '__main__':
     print(showtime_dict, prod_info_dict, sep='\n\n')
 
     # Quit and close the driver, to conclude.
-    driver.close()
+    driver.quit()
--- its_showtimes/scrapers/siskel_scrape.py
+++ its_showtimes/scrapers/siskel_scrape.py
@@ -44,18 +44,32 @@ def siskel_scrape(driver: webdriver.Chrome,
     siskel_link = 'https://www.siskelfilmcenter.org/calendar'
     driver.get(siskel_link)
     calendar_link_xpath = '/html/body/div[1]/div[4]/div/section/div[2]/article/div/div/p/a'
-    driver.find_element(By.XPATH, calendar_link_xpath).click()
+    true_calendar_element = driver.find_element(By.XPATH, calendar_link_xpath)
+    true_calendar_link = true_calendar_element.get_attribute('href')
+    driver.get(true_calendar_link)
+    # driver.find_element(By.XPATH, calendar_link_xpath).click()
 
-    # Now at the actual calendar, take its HTML text, and create a
-    # BeautifulSoup object from it.
-    calendar_xpath = '//*[@id="ctl00_CPH1_EventCalendar_CalendarTable"]/tbody'
-    calendar_text = driver.find_element(By.XPATH, calendar_xpath
-                                        ).get_attribute('innerHTML')
-    soup = BeautifulSoup(calendar_text, 'html.parser')
+    # Now at the actual calendar, we check for a working link to the
+    # next month's page, by checking for a clickable "Next" button.
+    next_month_button_xpath = '//*[@id="ctl00_CPH1_EventCalendar_calNext"]'
+    next_month_button_element = driver.find_element(By.XPATH, 
+                                                    next_month_button_xpath)
+    
+    calendar_links = [true_calendar_link]
 
-    # Form an iterable of all the calendar's days, by selecting such
-    # elements from the Soup.
-    calendar_days = soup.select('td.InsideDate')
+    if next_month_button_element:
+        
+        driver.implicitly_wait(3)
+        next_month_button_element.click()
+        driver.implicitly_wait(3)
+        calendar_links.append(driver.current_url)
+        print(driver.current_url)
+
+        driver.get(true_calendar_link)
+
+        # next_month_cal_link = next_month_button_element.get_attribute('href')
+        # print(next_month_cal_link)
+        # calendar_links.append(next_month_cal_link)
 
     # Initialize various dictionaries
     films_showtimes = {} # Tracks each film's showtimes
@@ -70,83 +84,97 @@ def siskel_scrape(driver: webdriver.Chrome,
     #                       # Siskel, which appear to be indicated by a
     #                       # colon in the show title.
 
-    # Iterate through each of the calendar's days in order to collect
-    # data on the shows' times and films.
-    for day in calendar_days:
+    for cal_link in calendar_links:
+        driver.get(cal_link)
 
-        # Iterate through this day's shows.
-        days_films = day.select('div.Item')
-        for film in days_films:
+        # For this calendar page, get the HTML text and create a
+        # BeautifulSoup object from it.
+        calendar_xpath = '//*[@id="ctl00_CPH1_EventCalendar_CalendarTable"]/tbody'
+        calendar_text = driver.find_element(By.XPATH, calendar_xpath
+                                            ).get_attribute('innerHTML')
+        soup = BeautifulSoup(calendar_text, 'html.parser')
 
-            # Get the show's name, and parse it into the names of the
-            # screened film and series (if applicable.)
-            show_name = film.select_one('div.Name').text
-            film_name, show_series_prepends = parse_show_name(show_name)
-            # print(show_name, film_name, show_series_prepends, sep='\t||\t')
+        # Form an iterable of all the calendar's days, by selecting such
+        # elements from the Soup.
+        calendar_days = soup.select('td.InsideDate')
 
-            # If this show is indeed part of a series, log the series'
-            # name in a dictionary.
-            if show_series_prepends:
-                primary_series = show_series_prepends[0]
-                series_prepends_str = ': '.join(show_series_prepends)
-                # print(primary_series, series_prepends_str, film_name, sep='\t||||\t')
-                if primary_series not in siskel_series_dict:
-                    siskel_series_dict[primary_series] = [(film_name, series_prepends_str)]
-                else:
-                    siskel_series_dict[primary_series].append([(film_name, series_prepends_str)])
+        # Iterate through each of the calendar's days in order to collect
+        # data on the shows' times and films.
+        for day in calendar_days:
 
-            # Iterate through this film's showtimes that day
-            films_showtimes_that_day = film.select('div.Time')
-            for showtime in films_showtimes_that_day:
-                # Retrieve the string that indicates the showtime and
-                # parse it into a datetime object.
-                showtime_str = showtime.get('data-agl_date') + 'M'
-                showtime_datetime = datetime.strptime(showtime_str, '%m/%d/%y %I:%M %p')
+            # Iterate through this day's shows.
+            days_films = day.select('div.Item')
+            for film in days_films:
 
-                # Add this showtime to a dictionary of them, using the
-                # film's name as the key:
+                # Get the show's name, and parse it into the names of the
+                # screened film and series (if applicable.)
+                show_name = film.select_one('div.Name').text
+                film_name, show_series_prepends = parse_show_name(show_name)
+                # print(show_name, film_name, show_series_prepends, sep='\t||\t')
 
-                # If this film hasn't yet had any showtimes logged,
-                # first retrieve a few of its production details from
-                # its "MORE INFO" page on the Siskel site.
-                if film_name not in films_showtimes:
+                # If this show is indeed part of a series, log the series'
+                # name in a dictionary.
+                if show_series_prepends:
+                    primary_series = show_series_prepends[0]
+                    series_prepends_str = ': '.join(show_series_prepends)
+                    # print(primary_series, series_prepends_str, film_name, sep='\t||||\t')
+                    if primary_series not in siskel_series_dict:
+                        siskel_series_dict[primary_series] = [(film_name, series_prepends_str)]
+                    else:
+                        siskel_series_dict[primary_series].append([(film_name, series_prepends_str)])
 
-                    # Navigate to the film's "MORE INFO" page.
-                    # link_to_details_page = film.select_one('a.agl-epgbutton').get('href')
-                    link_to_details_page = film.select_one('a.ViewLink').get('href')
-                    link_to_details_page = 'https://prod5.agileticketing.net/websales/pages/' + link_to_details_page
-                    driver.get(link_to_details_page)
+                # Iterate through this film's showtimes that day
+                films_showtimes_that_day = film.select('div.Time')
+                for showtime in films_showtimes_that_day:
+                    # Retrieve the string that indicates the showtime and
+                    # parse it into a datetime object.
+                    showtime_str = showtime.get('data-agl_date') + 'M'
+                    showtime_datetime = datetime.strptime(showtime_str, '%m/%d/%y %I:%M %p')
 
-                    # Instantiate a dictionary for just this film's
-                    # production details.
-                    this_films_details = {}
+                    # Add this showtime to a dictionary of them, using the
+                    # film's name as the key:
 
-                    # Iterate through the production details shown on
-                    # that page, taking only those named in the below
-                    # 'if/elif' branches. At time of writing, these
-                    # include release year, runtime, and director.
-                    film_detail_elems = driver.find_elements(By.CSS_SELECTOR, 'span.PropValue')
-                    for detail_elem in film_detail_elems:
-                        if detail_elem.get_attribute('cpropname').strip() == 'Release Year':
-                            film_release_yr = detail_elem.get_attribute('innerHTML')
-                            this_films_details['Release Year'] = film_release_yr
+                    # If this film hasn't yet had any showtimes logged,
+                    # first retrieve a few of its production details from
+                    # its "MORE INFO" page on the Siskel site.
+                    if film_name not in films_showtimes:
+
+                        # Navigate to the film's "MORE INFO" page.
+                        # link_to_details_page = film.select_one('a.agl-epgbutton').get('href')
+                        link_to_details_page = film.select_one('a.ViewLink').get('href')
+                        link_to_details_page = 'https://prod5.agileticketing.net/websales/pages/' + link_to_details_page
+                        driver.get(link_to_details_page)
+
+                        # Instantiate a dictionary for just this film's
+                        # production details.
+                        this_films_details = {}
+
+                        # Iterate through the production details shown on
+                        # that page, taking only those named in the below
+                        # 'if/elif' branches. At time of writing, these
+                        # include release year, runtime, and director.
+                        film_detail_elems = driver.find_elements(By.CSS_SELECTOR, 'span.PropValue')
+                        for detail_elem in film_detail_elems:
+                            if detail_elem.get_attribute('cpropname').strip() == 'Release Year':
+                                film_release_yr = detail_elem.get_attribute('innerHTML')
+                                this_films_details['Release Year'] = film_release_yr
+                                
+                            elif detail_elem.get_attribute('cpropname').strip() == 'Runtime':
+                                film_runtime = detail_elem.get_attribute('innerHTML')
+                                film_runtime = int(film_runtime)
+                                this_films_details['Runtime'] = film_runtime
                             
-                        elif detail_elem.get_attribute('cpropname').strip() == 'Runtime':
-                            film_runtime = detail_elem.get_attribute('innerHTML')
-                            film_runtime = int(film_runtime)
-                            this_films_details['Runtime'] = film_runtime
-                        
-                        elif detail_elem.get_attribute('cpropname').strip() == 'Director':
-                            film_director = detail_elem.get_attribute('innerHTML')
-                            this_films_details['Director'] = film_director
-                        
-                    films_showtimes[film_name] = [showtime_datetime]
-                    film_details[film_name] = this_films_details
+                            elif detail_elem.get_attribute('cpropname').strip() == 'Director':
+                                film_director = detail_elem.get_attribute('innerHTML')
+                                this_films_details['Director'] = film_director
+                            
+                        films_showtimes[film_name] = [showtime_datetime]
+                        film_details[film_name] = this_films_details
 
-                    # print(f'{film_name} added first-time. Details: {this_films_details}')
+                        # print(f'{film_name} added first-time. Details: {this_films_details}')
 
-                else:
-                    films_showtimes[film_name].append(showtime_datetime)
+                    else:
+                        films_showtimes[film_name].append(showtime_datetime)
 
 
     # # (For debugging/feedback) Print the various dictionaries.
