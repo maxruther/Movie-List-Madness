65127e38b93078264fa64b24a0ec3c8499b1cade
maxruther <34466604+maxruther@users.noreply.github.com>
2025-04-03 20:29:46 -0500

    Conceal creds in 'build_movielist_db.py'
    Now creds are read in from an untracked file, rather than hard-coded.

--- build_movielist_db.py
+++ build_movielist_db.py
@@ -1,14 +1,22 @@
 import pymysql
+from sqlalchemy import create_engine
+
 from movielist_ingestion.evernote_to_mysql import evernote_to_mysql
 from omdb_builder.build_out_omdb import build_out_omdb_tables
 from critic_ratings.fix_critic_ratings_tbl import fix_critic_ratings_tbl
 
 # Connect to my MySQL movie database
+
+# Read in my database's creds/URL from a file.
+host, user, password, database = None, None, None, None
+with open('.secret/movie_db_creds_pymysql.txt', 'r') as f:
+    host, user, password, database = f.read().strip().split('\n')
+
 my_db = pymysql.connect(
-    host="localhost",
-    user="root",
-    password="yos",
-    database="movieDB"
+    host=host,
+    user=user,
+    password=password,
+    database=database,
 )
 
 # Specify filepath of the MovieList html file, as exported from
@@ -30,11 +38,12 @@ build_out_omdb_tables('load from file', my_db)
 # specify the database's filepath, to reconnect via SQLAlchemy, a method
 # more advantageous to a method closely following.
 my_db.close()
-db_path = 'mysql://root:yos@localhost/moviedb'
+# Read in my database's creds/URL from a file.
+movie_db_url = f"mysql://{user}:{password}@{host}/{database}"
 
 # Implement some fixes to the table of critics' movie ratings. These
 # fixes include filling in some scores that were erroneously missed by
 # OMDB, as well as adding additional reviewers' scores, like from
 # RogerEbert.com .
-fix_critic_ratings_tbl(db_path)
-# fix_critic_ratings_tbl(db_path, na_report=True)
+# fix_critic_ratings_tbl(movie_db_url)
+fix_critic_ratings_tbl(movie_db_url, na_report=True)
